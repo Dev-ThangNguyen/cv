c39edce790cb7cb38e337af329f80e36ec0b0eef
Thang Nguyen <thang1997iuh@gmail.com>
2022-01-04 14:24:17 +0700

    firsts commits

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// const avatar = document.querySelector('.avatar');
+// if(document.documentElement.clientWidth > 1020) {
+//     console.log(123)
+//     avatar.src = './stogare/avarta.jpg'
+// }
